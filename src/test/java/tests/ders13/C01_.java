4ab4d07da54b336860bf6001b2769dc8c768cecd
BurhanSemiz <77208168+burhansemiz2020@users.noreply.github.com>
2023-04-03 01:09:10 +0200

    new adds2

---
+++ src/test/java/tests/ders13/C01_.java
@@ -0,0 +1,10 @@
+package tests.ders13;
+
+import org.testng.annotations.Test;
+
+public class C01_ {
+    @Test
+    public void test01() {
+
+    }
+}
---
+++ src/test/java/tests/ders14/C01_.java
@@ -0,0 +1,10 @@
+package tests.ders14;
+
+import org.testng.annotations.Test;
+
+public class C01_ {
+    @Test
+    public void test01() {
+
+    }
+}
