3a52fdad15f4f5f528cc45f6e024078a279bddd7
BurhanSemiz <77208168+burhansemiz2020@users.noreply.github.com>
2023-04-05 01:40:32 +0200

    TestNG_05-04-2023-V.2

---
+++ src/test/java/tests/ders13/C02_RaporluPozitifLoginTest.java
@@ -0,0 +1,51 @@
+package tests.ders13;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.TestBaseRapor;
+
+public class C02_RaporluPozitifLoginTest extends TestBaseRapor {
+    /*
+        Yeni hedefimiz test datalarini manuel olarak girmek degil
+        test datalarini configuration.properties dosyasindan alip kullanmak
+        Boylece test datalarinda herhangi bir update yapilmasi istendiginde
+        sadece configuration.properties'deki degeri degistirerek
+        tum testlerimizi update edebilmek
+     */
+
+    @Test
+    public void test01() throws InterruptedException {
+        extentTest=extentReports.createTest("Pozitif Login Testi", "Dogru bilgilerle giris yapilabilmeli");
+        // Bir test methodu olusturun
+        // qualitydemy anasayfasina gidin
+        Driver.getDriver().get(ConfigReader.getProperty("qdUrl"));
+        extentTest.info("qualityDemy anasayfaya gidildi");
+        // cookies'i kabul edin
+        QualitydemyPage qualitydemyPage = new QualitydemyPage();
+        qualitydemyPage.cookiesKabulButonu.click();
+        extentTest.info("cookies kabul edildi");
+        Thread.sleep(2000);
+        // Login linkine tiklayin
+        qualitydemyPage.firstLoginLink.click();
+        extentTest.info("ilk login linkine tiklandi");
+        Thread.sleep(2000);
+        // gecerli kullanici adi ve gecerli password yazip
+
+        qualitydemyPage.loginEmailBox.sendKeys(ConfigReader.getProperty("qdGecerliUserName"));
+        qualitydemyPage.loginPasswordBox.sendKeys(ConfigReader.getProperty("qdGecerliPassword"));
+        extentTest.info("gecerli kullanici adi ve sifre girildi");
+
+        // login butonuna bastiginizda
+        qualitydemyPage.loginButton.click();
+        extentTest.info("login butonuna basildi");
+        // basarili olarak giris yapildigini test edin
+        Assert.assertTrue(qualitydemyPage.successEnterElement.isEnabled());
+        extentTest.info("basarili sekilde giris yapildigi test edildi");
+        Thread.sleep(3000);
+        Driver.closeDriver();
+
+    }
+}
